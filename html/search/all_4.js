f53371f6ef589777a1062685937bc9473e939d52
greyishsong <greyishsong@stu.xjtu.edu.cn>
2023-08-01 18:33:07 +0800

    update on 2023-08-01 18:33:11+08:00

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,18 +1,17 @@
 var searchData=
 [
-  ['edge_0',['Edge',['../d2/d4f/structEdge.html#afe6670299d7620b455b0f7a589236637',1,'Edge']]],
-  ['edge_1',['edge',['../df/d02/structHalfedge.html#a0465cf62d0c9580ba6739bf1b73e76e3',1,'Halfedge::edge()'],['../d8/d88/structGL_1_1Mesh.html#afbd5496ce294b47446f8af09fe33dbb8',1,'GL::Mesh::edge()']]],
-  ['edge_2',['Edge',['../d2/d4f/structEdge.html',1,'']]],
-  ['edgerecord_3',['EdgeRecord',['../d7/d4b/structHalfedgeMesh_1_1EdgeRecord.html',1,'HalfedgeMesh']]],
-  ['edges_4',['edges',['../dc/d6d/classHalfedgeMesh.html#aa6f5b6c2e48ecbf535fc116656dad2bb',1,'HalfedgeMesh']]],
-  ['elementarraybuffer_5',['ElementArrayBuffer',['../da/dd6/structGL_1_1ElementArrayBuffer.html#a2f7352881659cbf5f07d9026409dac29',1,'GL::ElementArrayBuffer::ElementArrayBuffer()'],['../da/dd6/structGL_1_1ElementArrayBuffer.html',1,'GL::ElementArrayBuffer&lt; size &gt;']]],
-  ['elementarraybuffer_3c_202_20_3e_6',['ElementArrayBuffer&lt; 2 &gt;',['../da/dd6/structGL_1_1ElementArrayBuffer.html',1,'GL']]],
-  ['elementarraybuffer_3c_203_20_3e_7',['ElementArrayBuffer&lt; 3 &gt;',['../da/dd6/structGL_1_1ElementArrayBuffer.html',1,'GL']]],
-  ['erase_8',['erase',['../dc/d6d/classHalfedgeMesh.html#a462ef58b4659d53a6555f6893278673a',1,'HalfedgeMesh::erase()'],['../d7/dde/classLinkedList.html#a4cb923f17adcd74735d50eb9d2b46a19',1,'LinkedList::erase()'],['../dc/d6d/classHalfedgeMesh.html#a2dddd55f43333c9bad1ca7465661ad29',1,'HalfedgeMesh::erase(Face *f)'],['../dc/d6d/classHalfedgeMesh.html#ac18e983e0daabc0560e5e026d1b536d3',1,'HalfedgeMesh::erase(Edge *e)'],['../dc/d6d/classHalfedgeMesh.html#a88c7b2cd47aa7f4683e29b12bc65f6f2',1,'HalfedgeMesh::erase(Vertex *v)']]],
-  ['erased_5fedges_9',['erased_edges',['../dc/d6d/classHalfedgeMesh.html#a37dce586765de694d13b82c6cd81ea7b',1,'HalfedgeMesh']]],
-  ['erased_5ffaces_10',['erased_faces',['../dc/d6d/classHalfedgeMesh.html#ae2be3f28913ee5fca0afaa606fac56d5',1,'HalfedgeMesh']]],
-  ['erased_5fhalfedges_11',['erased_halfedges',['../dc/d6d/classHalfedgeMesh.html#a243bb83b937892df15018e298e3061de',1,'HalfedgeMesh']]],
-  ['erased_5fvertices_12',['erased_vertices',['../dc/d6d/classHalfedgeMesh.html#a3a8dae5c68bd6d7ed91b13c717bdd891',1,'HalfedgeMesh']]],
-  ['error_5finfo_13',['error_info',['../dc/d6d/classHalfedgeMesh.html#a3e4d4472c21ee0c5bb36dedecafd0857',1,'HalfedgeMesh']]],
-  ['eventloop_14',['eventloop',['../df/d1e/classPlatform.html#a623e404af56fbf153dcaa64214404f9c',1,'Platform']]]
+  ['edge_0',['edge',['../df/d02/structHalfedge.html#a0465cf62d0c9580ba6739bf1b73e76e3',1,'Halfedge::edge()'],['../d8/d88/structGL_1_1Mesh.html#afbd5496ce294b47446f8af09fe33dbb8',1,'GL::Mesh::edge()']]],
+  ['edge_1',['Edge',['../d2/d4f/structEdge.html#afe6670299d7620b455b0f7a589236637',1,'Edge::Edge()'],['../d2/d4f/structEdge.html',1,'Edge']]],
+  ['edgerecord_2',['EdgeRecord',['../d7/d4b/structHalfedgeMesh_1_1EdgeRecord.html',1,'HalfedgeMesh']]],
+  ['edges_3',['edges',['../dc/d6d/classHalfedgeMesh.html#aa6f5b6c2e48ecbf535fc116656dad2bb',1,'HalfedgeMesh']]],
+  ['elementarraybuffer_4',['ElementArrayBuffer',['../da/dd6/structGL_1_1ElementArrayBuffer.html#a2f7352881659cbf5f07d9026409dac29',1,'GL::ElementArrayBuffer::ElementArrayBuffer()'],['../da/dd6/structGL_1_1ElementArrayBuffer.html',1,'GL::ElementArrayBuffer&lt; size &gt;']]],
+  ['elementarraybuffer_3c_202_20_3e_5',['ElementArrayBuffer&lt; 2 &gt;',['../da/dd6/structGL_1_1ElementArrayBuffer.html',1,'GL']]],
+  ['elementarraybuffer_3c_203_20_3e_6',['ElementArrayBuffer&lt; 3 &gt;',['../da/dd6/structGL_1_1ElementArrayBuffer.html',1,'GL']]],
+  ['erase_7',['erase',['../dc/d6d/classHalfedgeMesh.html#a462ef58b4659d53a6555f6893278673a',1,'HalfedgeMesh::erase()'],['../d7/dde/classLinkedList.html#a4cb923f17adcd74735d50eb9d2b46a19',1,'LinkedList::erase()'],['../dc/d6d/classHalfedgeMesh.html#a2dddd55f43333c9bad1ca7465661ad29',1,'HalfedgeMesh::erase(Face *f)'],['../dc/d6d/classHalfedgeMesh.html#ac18e983e0daabc0560e5e026d1b536d3',1,'HalfedgeMesh::erase(Edge *e)'],['../dc/d6d/classHalfedgeMesh.html#a88c7b2cd47aa7f4683e29b12bc65f6f2',1,'HalfedgeMesh::erase(Vertex *v)']]],
+  ['erased_5fedges_8',['erased_edges',['../dc/d6d/classHalfedgeMesh.html#a37dce586765de694d13b82c6cd81ea7b',1,'HalfedgeMesh']]],
+  ['erased_5ffaces_9',['erased_faces',['../dc/d6d/classHalfedgeMesh.html#ae2be3f28913ee5fca0afaa606fac56d5',1,'HalfedgeMesh']]],
+  ['erased_5fhalfedges_10',['erased_halfedges',['../dc/d6d/classHalfedgeMesh.html#a243bb83b937892df15018e298e3061de',1,'HalfedgeMesh']]],
+  ['erased_5fvertices_11',['erased_vertices',['../dc/d6d/classHalfedgeMesh.html#a3a8dae5c68bd6d7ed91b13c717bdd891',1,'HalfedgeMesh']]],
+  ['error_5finfo_12',['error_info',['../dc/d6d/classHalfedgeMesh.html#a3e4d4472c21ee0c5bb36dedecafd0857',1,'HalfedgeMesh']]],
+  ['eventloop_13',['eventloop',['../df/d1e/classPlatform.html#a623e404af56fbf153dcaa64214404f9c',1,'Platform']]]
 ];
